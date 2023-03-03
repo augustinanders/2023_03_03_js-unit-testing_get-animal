5769c2d338dccc97b036596c0753fc69bb1f83d3
Augustin Anders <augustinanders@gmail.com>
2023-03-03 12:05:43 +0100

    fix bugs

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ export function getAnimal(animal) {
     return "I totally love cats!";
   } else if (animal === "dogs") {
     return "I like dogs!";
+  } else if (animal === undefined) {
+    return "I do not like animals at all!";
   }
-  return "I do not like animals at all!";
+  return `I like ${animal}`;
 }
