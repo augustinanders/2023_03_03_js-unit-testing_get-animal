93c467a7b447008b2e9fed57574bc3d1db21342d
Augustin Anders <augustinanders@gmail.com>
2023-03-03 11:24:36 +0100

    fix function

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  if (animal === "cats") {
+    return "I totally love cats!";
+  } else if (animal === "dogs") {
+    return "I like dogs!";
+  }
+  return "I do not like animals at all!";
+}
